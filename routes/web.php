e37ec691cd001fc990bbffd3473cc101c03aad9e
Jerson Josue <gersonnm08@gmail.com>
2023-12-16 14:10:09 -0600

    Es una nueva subida

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/', function () {
-    return Redirect::to('login');
+    return view('welcome');
 });
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
